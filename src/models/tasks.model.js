aa18a99c74341ea09f887df156382f0598b1a979
Clocky97 <milagrosruizdiazz150@gmail.com>
2025-08-17 23:06:15 -0300

    código en models modificado con la identación dada en el repaso

--- src/models/tasks.model.js
+++ src/models/tasks.model.js
@@ -1,9 +1,28 @@
-import {sequelize} from "../config/database.js";
+import { sequelize } from "../config/database.js";
 import { DataTypes } from "sequelize";
 
 export const Tasks = sequelize.define("tasks", {
-    id: {primaryKey: true, allowNull: false, type: DataTypes.INTEGER, autoIncrement: true},
-    title: {type: DataTypes.STRING(100), allowNull: false, unique: true},
-    description: {type: DataTypes.STRING(100), allowNull: false},
-    isComplete: {type: DataTypes.BOOLEAN, default: false}
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  description: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  isComplete: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,9 +1,24 @@
-import {sequelize} from "../config/database.js";
+import { sequelize } from "../config/database.js";
 import { DataTypes } from "sequelize";
 
 export const Users = sequelize.define("users", {
-    id: {primaryKey: true, allowNull: false, type: DataTypes.INTEGER, autoIncrement: true},
-    name: {type: DataTypes.STRING(100), allowNull: false},
-    email: {type: DataTypes.STRING(100), allowNull: false, unique: true},
-    password: {type: DataTypes.STRING(100), allowNull: false}
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
 });
